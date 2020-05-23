92dcc7813ab972d7e6dce94e6ae6c62754374c4f
Jake Hamilton <jakehamtexas@gmail.com>
2020-05-23 15:53:11 -0500

    Catches up route_builder and home_body to new naming convention for widget

--- lib/home/home_body.dart
+++ lib/home/home_body.dart
@@ -1,6 +1,6 @@
+import 'package:better_c25k/regimen/regimen.dart';
 import 'package:flutter/material.dart';
 
-import 'package:better_c25k/regimen/model/regimen.dart';
 import 'package:better_c25k/router/routes.dart';
 
 class HomeBody extends StatelessWidget {
@@ -8,8 +8,8 @@ class HomeBody extends StatelessWidget {
   Widget build(BuildContext context) => Container(
         child: RaisedButton(
           child: Text("Go To C25k"),
-          onPressed: () => Navigator.of(context).pushNamed(Routes.regimen,
-              arguments: C25KRegimenModel()),
+          onPressed: () => Navigator.of(context)
+              .pushNamed(Routes.regimen, arguments: C25KRegimenModel()),
         ),
       );
 }
--- lib/router/route_builders.dart
+++ lib/router/route_builders.dart
@@ -6,7 +6,7 @@ import '../regimen/regimen.dart';
 import '../home/home.dart';
 
 final _home = (Object _args) => (BuildContext _) => Home();
-final _regimen = (Object args) => (BuildContext _) => Regimen(args);
+final _regimen = (Object args) => (BuildContext _) => RegimenPage(args);
 
 final Map<String, Widget Function(BuildContext) Function(Object)>
     routeBuilders = {Routes.home: _home, Routes.regimen: _regimen};
