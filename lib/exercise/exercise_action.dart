6f8c3a70693179d6bd464c86e6d2bf77a2df9d70
Jake Hamilton <jakehamtexas@gmail.com>
2020-05-24 22:19:19 -0500

    Adds extension for ExerciseAction which allows usage of a message for the corresponding enum value.

--- lib/exercise/exercise_action.dart
+++ lib/exercise/exercise_action.dart
@@ -2,3 +2,14 @@
 /// workout action, like a warmup or cooldown, or an in-progress workout
 /// action, like walking or running.
 enum ExerciseAction { warmup, walk, run, cooldown }
+
+extension ExerciseActionMessage on ExerciseAction {
+  static const Map<ExerciseAction, String> _messagesByAction = {
+    ExerciseAction.warmup: "Warmup",
+    ExerciseAction.walk: "Walk",
+    ExerciseAction.run: "Run",
+    ExerciseAction.cooldown: "Cooldown",
+  };
+  String get message =>
+      _messagesByAction.containsKey(this) ? _messagesByAction[this] : "Unknown";
+}
