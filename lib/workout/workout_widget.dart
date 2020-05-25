026afa19088e82e589dc542f838a11e92830d21b
Jake Hamilton <jakehamtexas@gmail.com>
2020-05-25 13:58:03 -0500

    Refactors workout_widget and implements bloc pattern

--- lib/workout/workout_widget.dart
+++ lib/workout/workout_widget.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../router/routes.dart';
+import 'bloc/workout_bloc.dart';
 import 'do_workout/do_workout.dart';
+import 'workout_card.dart';
 import 'workout_model.dart';
 
-class WorkoutWidgetState extends State<WorkoutWidget> {
+class WorkoutWidget extends StatelessWidget {
   final WorkoutModel _workout;
   String get _workoutDescription => _workout.description;
-  WorkoutWidgetState(this._workout);
   String get _workoutTitle {
     final weekNumber = _workout.ordinalWeekNumber;
     final week = "Week $weekNumber";
@@ -17,135 +19,52 @@ class WorkoutWidgetState extends State<WorkoutWidget> {
     return "$week $day";
   }
 
-  bool _isExpanded = false;
-  void _toggleExpand() {
-    setState(() {
-      _isExpanded = !_isExpanded;
-    });
-  }
-
-  void _start() {
-    Navigator.of(context).pushNamed(
-      Routes.doWorkout,
-      arguments: DoWorkoutConfiguration(
-        exercises: _workout.exercises,
-        workoutTitle: _workoutTitle,
-      ),
-    );
-  }
+  WorkoutWidget(this._workout);
 
   @override
   Widget build(BuildContext context) {
-    final double _height = _isExpanded ? 155 : 72;
-    final _duration = Duration(milliseconds: 350);
-    final _card = _WorkoutCard(
-      isExpanded: _isExpanded,
-      start: _start,
-      toggleExpand: _toggleExpand,
-      workoutDescription: _workoutDescription,
-      workoutTitle: _workoutTitle,
-    );
-    return Center(
-      child: AnimatedContainer(
-        duration: _duration,
-        height: _height,
-        child: _card,
-      ),
-    );
-  }
-}
-
-class WorkoutWidget extends StatefulWidget {
-  final WorkoutModel _workout;
-  WorkoutWidget(this._workout);
-  @override
-  State<StatefulWidget> createState() => WorkoutWidgetState(_workout);
-}
-
-class _WorkoutCard extends StatelessWidget {
-  final bool isExpanded;
-  final String workoutTitle;
-  final String workoutDescription;
-  final Function toggleExpand;
-  final Function start;
-  _WorkoutCard({
-    @required this.isExpanded,
-    @required this.workoutTitle,
-    @required this.workoutDescription,
-    @required this.toggleExpand,
-    @required this.start,
-  });
-  Padding _verticalPadding(Widget child) {
-    const verticalPadding = EdgeInsets.fromLTRB(0, 10, 0, 10);
-    return Padding(
-      padding: verticalPadding,
-      child: child,
-    );
-  }
-
-  Padding _horizontalPadding(Widget child) {
-    const horizontalPadding = EdgeInsets.fromLTRB(10, 0, 10, 0);
-    return Padding(
-      padding: horizontalPadding,
-      child: child,
-    );
-  }
-
-  Card _getCard() {
-    final workoutTitleWidget = Text(
-      workoutTitle,
-      style: TextStyle(fontWeight: FontWeight.bold),
-    );
-    final buttonBar = ButtonBar(
-      children: <Widget>[
-        RaisedButton(
-          child: Text(isExpanded ? "Collapse" : "Expand"),
-          onPressed: toggleExpand,
+    final start = () {
+      Navigator.of(context).pushNamed(
+        Routes.doWorkout,
+        arguments: DoWorkoutConfiguration(
+          exercises: _workout.exercises,
+          workoutTitle: _workoutTitle,
         ),
-        RaisedButton(
-          child: Text("Start"),
-          onPressed: start,
-        )
-      ],
-    );
+      );
+    };
+    final duration = Duration(milliseconds: 350);
+    return BlocProvider<WorkoutBloc>(
+      builder: (_) => WorkoutBloc(),
+      child: Center(
+        child: BlocBuilder<WorkoutBloc, WorkoutState>(
+          builder: (BuildContext context, state) {
+            if (state is WorkoutExpandToggledOn) {
+              return AnimatedContainer(
+                duration: duration,
+                height: 155,
+                child: ExpandedWorkoutCard(
+                  startFunc: start,
+                  workoutDescription: _workoutDescription,
+                  workoutTitle: _workoutTitle,
+                ),
+              );
+            }
 
-    final withoutDescription = _horizontalPadding(
-      Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              workoutTitleWidget,
-              buttonBar,
-            ],
-          ),
-        ],
+            if (state is WorkoutExpandToggledOff) {
+              return AnimatedContainer(
+                duration: duration,
+                height: 72,
+                child: CollapsedWorkoutCard(
+                  startFunc: start,
+                  workoutDescription: _workoutDescription,
+                  workoutTitle: _workoutTitle,
+                ),
+              );
+            }
+            return null;
+          },
+        ),
       ),
-    );
-
-    final withDescription = _horizontalPadding(
-      Wrap(
-        children: [
-          Column(
-            children: <Widget>[
-              _verticalPadding(
-                workoutTitleWidget,
-              ),
-              Text(workoutDescription),
-            ],
-          ),
-          buttonBar,
-        ],
-      ),
-    );
-
-    final cardChild = isExpanded ? withDescription : withoutDescription;
-
-    return Card(
-      child: cardChild,
     );
   }
-
-  @override
-  Widget build(BuildContext context) => _getCard();
 }
