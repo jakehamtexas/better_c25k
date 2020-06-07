63b17af849cc9d31c2e954967d93d4cbf9b92f31
Jake Hamilton <jakehamtexas@gmail.com>
2020-06-06 22:51:37 -0500

    Adds get_workouts usecase

---
+++ lib/domain/usecases/get_workouts.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+
+import '../../core/error/failure.dart';
+import '../../domain/repository/repository.dart';
+import '../entities/workout/workout.dart';
+
+class GetWorkouts {
+  final WorkoutRepository _repository;
+  GetWorkouts(this._repository);
+
+  Future<Either<Failure, List<WorkoutPresentationEntity>>> call(
+      int regimenId) async {
+    return await _repository.getWorkoutsForRegimenId(regimenId);
+  }
+}
