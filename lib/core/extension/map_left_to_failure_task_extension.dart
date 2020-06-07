9bac192f5a1576c09e2647360d37a02e2bc3b506
Jake Hamilton <jakehamtexas@gmail.com>
2020-06-06 19:56:30 -0500

    Simplifies mapLeftToFailure

--- lib/core/extension/map_left_to_failure_task_extension.dart
+++ lib/core/extension/map_left_to_failure_task_extension.dart
@@ -3,12 +3,12 @@ import 'package:dartz/dartz.dart';
 import '../error/error.dart';
 
 extension MapLeftToFailureTaskExtension<TEither extends Either<Object, TRight>,
-    TRight, TFailure extends Failure> on Task<TEither> {
-  Task<Either<TFailure, TRight>> mapLeftToFailure() {
+    TRight> on Task<TEither> {
+  Task<Either<Failure, TRight>> mapLeftToFailure() {
     return this.map(
       (either) => either.leftMap((obj) {
         try {
-          return obj as TFailure;
+          return obj as Failure;
         } catch (e) {
           throw obj;
         }
