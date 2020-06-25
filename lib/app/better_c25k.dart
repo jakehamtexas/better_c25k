6791029e1fdec470c511f5c86eff7b3a1e479ae7
Jake Hamilton <jakehamtexas@gmail.com>
2020-06-24 20:23:09 -0500

    Adds a theme - surprise, it's a dark theme!

--- lib/app/better_c25k.dart
+++ lib/app/better_c25k.dart
@@ -4,9 +4,16 @@ import '../presentation/router/route_generator.dart';
 
 class BetterC25K extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => const MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
         title: "Better C25K",
         initialRoute: '/',
         onGenerateRoute: RouteGenerator.generateRoute,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.blueGrey,
+          primarySwatch: Colors.blueGrey,
+          buttonTheme: const ButtonThemeData(textTheme: ButtonTextTheme.accent),
+          accentColor: Colors.white,
+        ),
       );
 }
