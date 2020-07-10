2d1ffc8932460576331eefbea3efc7c439fa6af7
Jake Hamilton <jakehamtexas@gmail.com>
2020-07-10 18:59:06 -0500

    Updates path for go_back_to_workouts_button.dart

--- lib/presentation/common/common.dart
+++ lib/presentation/common/common.dart
@@ -1,4 +1,4 @@
 export 'abstractions/has_page_index.dart';
 export 'widgets/big_workout_text.dart';
 export 'widgets/expanded_center.dart';
-export 'widgets/go_back_to_workouts_button.dart';
+export '../regimen/workout/go_back_to_workouts_button.dart';
