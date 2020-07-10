46ef47447e1a9ff80fe57bc82b71522dda383680
Jake Hamilton <jakehamtexas@gmail.com>
2020-07-10 18:26:08 -0500

    Adds usage of preferences bloc with builder and provides widget stubs

--- lib/presentation/preferences/preferences_page.dart
+++ lib/presentation/preferences/preferences_page.dart
@@ -1,4 +1,7 @@
+import 'package:better_c25k/domain/entities/preferences/preferences_entity.dart';
+import 'package:better_c25k/presentation/preferences/bloc/preferences_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../common/common.dart';
 import '../scaffold/scaffold.dart';
@@ -9,11 +12,83 @@ class PreferencesPage extends StatelessWidget implements HasPageIndex {
   @override
   Widget build(BuildContext context) {
     return C25kLayout(
-      body: const Text("This is the preferences page"),
+      body: BlocConsumer<PreferencesBloc, PreferencesState>(
+        builder: (BuildContext context, PreferencesState state) {
+          switch (state.runtimeType) {
+            case GetFailedState:
+              return FailedToGetPreferences();
+            case ShownSaveableButtonBarState:
+              return PreferencesWithSaveBar(
+                (state as HydratedPreferencesState).preferences,
+              );
+            case HiddenSaveableButtonBarState:
+              return PreferencesWithoutSaveBar(
+                (state as HydratedPreferencesState).preferences,
+              );
+            default:
+              return Container();
+          }
+        },
+        listener: (BuildContext context, PreferencesState state) {
+          switch (state.runtimeType) {
+            case SaveFailedState:
+              Scaffold.of(context).showSnackBar(SaveFailedSnackBar());
+              break;
+            case SaveSuccessfulState:
+              Scaffold.of(context).showSnackBar(SaveSuccessfulSnackBar());
+          }
+        },
+      ),
       index: pageIndex,
     );
   }
 
   @override
   int get pageIndex => 2;
+}
+
+class SaveSuccessfulSnackBar extends SnackBar {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class SaveFailedSnackBar extends SnackBar {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class PreferencesWithoutSaveBar extends StatelessWidget {
+  final PreferencesEntity preferences;
+  const PreferencesWithoutSaveBar(this.preferences);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class PreferencesWithSaveBar extends StatelessWidget {
+  final PreferencesEntity preferences;
+  const PreferencesWithSaveBar(this.preferences);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class FailedToGetPreferences extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
